ccd811eeeaa0745aa5d1666eeb17b148a0ce273c
s195901 <s195901@student.dtu.dk>
2025-01-07 11:38:17 +0100

    asd

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,8 @@
 from torch.utils.data import Dataset
 
+from mlops_final_exercise.data import corrupt_mnist
+
+
 '''
 from mlops_final_exercise.data import MyDataset
 
@@ -11,4 +14,7 @@ def test_my_dataset():
 '''
 
 def test():
-    assert True
+    #train_set, test_set = corrupt_mnist()
+    assert isinstance(1, int)
+    #assert isinstance(train_set, tuple) and len(train_set) == 2 and isinstance(train_set[0], Dataset) and isinstance(train_set[1], Dataset)
+    #assert isinstance(test_set, tuple) and len(test_set) == 2 and isinstance(test_set[0], Dataset) and isinstance(test_set[1], Dataset)

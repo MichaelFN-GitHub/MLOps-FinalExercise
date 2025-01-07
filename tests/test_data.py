589a5bd77acf485a7c820c1be1bf8ca5e77444ea
s195901 <s195901@student.dtu.dk>
2025-01-07 11:57:34 +0100

    asd

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from torch.utils.data import Dataset
 
 from mlops_final_exercise.data import corrupt_mnist

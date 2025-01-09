32b493458a198660a473c431f051eda4e2eb320b
s195901 <s195901@student.dtu.dk>
2025-01-09 09:29:29 +0100

    After logging and profiling

--- src/mlops_final_exercise/train.py
+++ src/mlops_final_exercise/train.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
+import wandb
 
 from mlops_final_exercise.data import corrupt_mnist
 from mlops_final_exercise.model import MyAwesomeModel
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 
-def train(lr: float = 1e-4, batch_size: int = 64, epochs: int = 3) -> None:
+def train(lr: float = 1e-4, batch_size: int = 32, epochs: int = 3) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
+    
+    # Initialize logging
+    run = wandb.init(
+        project="corrupt_mnist",
+        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
+    )
 
     model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
@@ -24,6 +33,8 @@ def train(lr: float = 1e-4, batch_size: int = 64, epochs: int = 3) -> None:
     statistics = {"train_loss": [], "train_accuracy": []}
     for epoch in range(epochs):
         model.train()
+
+        preds, targets = [], []
         for i, (img, target) in enumerate(train_dataloader):
             img, target = img.to(DEVICE), target.to(DEVICE)
             optimizer.zero_grad()
@@ -36,11 +47,45 @@ def train(lr: float = 1e-4, batch_size: int = 64, epochs: int = 3) -> None:
             accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
             statistics["train_accuracy"].append(accuracy)
 
+            wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+
+            preds.append(y_pred.detach().cpu())
+            targets.append(target.detach().cpu())
+
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
+                # add a plot of the input images
+                images = wandb.Image(img[:5].detach().cpu(), caption="Input images")
+                wandb.log({"images": images})
+
+                # add a plot of histogram of the gradients
+                grads = torch.cat([p.grad.flatten() for p in model.parameters() if p.grad is not None], 0)
+                wandb.log({"gradients": wandb.Histogram(grads)})
+
+        preds = torch.cat(preds, 0)
+        targets = torch.cat(targets, 0)
+
+        # ROC CURVE HERE IF YOU WANT TO ADD IT
+
+
     print("Training complete")
+
+    final_accuracy = accuracy_score(targets, preds.argmax(dim=1))
+    final_precision = precision_score(targets, preds.argmax(dim=1), average="weighted")
+    final_recall = recall_score(targets, preds.argmax(dim=1), average="weighted")
+    final_f1 = f1_score(targets, preds.argmax(dim=1), average="weighted")
+
     torch.save(model.state_dict(), "models/model.pth")
+    artifact = wandb.Artifact(
+        name="corrupt_mnist_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": final_accuracy, "precision": final_precision, "recall": final_recall, "f1": final_f1},
+    )
+    artifact.add_file("models/model.pth")
+    run.log_artifact(artifact)
+
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     axs[0].plot(statistics["train_loss"])
     axs[0].set_title("Train loss")
